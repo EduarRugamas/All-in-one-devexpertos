52770c31ae95f0f31975071b075c3a60234da32f
joeldeleon25 <68795368+joeldeleon25@users.noreply.github.com>
2021-02-26 22:33:59 -0600

    pantalla ejercicio2

--- app/src/main/java/com/devexpertos/allinone/Ecuacion_Cuadratica/Ejercicio_1.kt
+++ app/src/main/java/com/devexpertos/allinone/Ecuacion_Cuadratica/Ejercicio_1.kt
@@ -2,11 +2,16 @@ package com.devexpertos.allinone.Ecuacion_Cuadratica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import com.devexpertos.allinone.R
 
 class Ejercicio_1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio_1)
+
+        val cantidad=findViewById<View>(R.id.buket) as EditText
+
     }
 }

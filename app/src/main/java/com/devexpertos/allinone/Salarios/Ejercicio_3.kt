76848c7fee95f61d467e7e9ff581c7904a60836a
joeldeleon25 <68795368+joeldeleon25@users.noreply.github.com>
2021-02-28 11:22:11 -0600

    ejercicio3 : Dropdown

--- app/src/main/java/com/devexpertos/allinone/Salarios/Ejercicio_3.kt
+++ app/src/main/java/com/devexpertos/allinone/Salarios/Ejercicio_3.kt
@@ -2,11 +2,48 @@ package com.devexpertos.allinone.Salarios
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 import com.devexpertos.allinone.R
+import kotlinx.android.synthetic.main.activity_ejercicio_3.*
 
 class Ejercicio_3 : AppCompatActivity() {
+
+    lateinit var option: Spinner
+    lateinit var cargo: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio_3)
+
+        //calcSueldo.setOnClickListener {
+        // Toast.makeText(this, "Los campos estan vacios", Toast.LENGTH_SHORT).show()
+        //}
+
+        option = findViewById(R.id.spinnerMenu) as Spinner
+        cargo = findViewById(R.id.cargo) as TextView
+
+        val options = arrayOf("Gerente", "Asistente", "Secretaria")
+
+        option.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,options)
+
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                cargo.text = options.get(position)
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                cargo.text = "Seleccione una opcion"
+            }
+
+        }
+
     }
 }
+
--- app/src/main/java/com/devexpertos/allinone/Votos/Ejercicio_2.kt
+++ app/src/main/java/com/devexpertos/allinone/Votos/Ejercicio_2.kt
@@ -1,17 +1,25 @@
 package com.devexpertos.allinone.Votos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import com.devexpertos.allinone.R
+import kotlinx.android.synthetic.main.activity_ejercicio_2.*
 
 class Ejercicio_2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio_2)
 
+        val bucket = buket.text
 
+        btnAgregar.setOnClickListener {
+            if (bucket.isEmpty())
+            Toast.makeText(this, "Los campos no deben estar vacios", Toast.LENGTH_SHORT).show()
 
+        }
     }
 }
